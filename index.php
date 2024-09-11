deebcefbe242063b5b26b137c836aae2ef19c120
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-09-11 12:13:37 +0300

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $marque = readline("entrer un marque de voiture : "); //readline() recupère le text et met dans le variable text 
+    $marque = readline("Ajoutez une marque de voiture : "); //readline() recupère le text et met dans le variable text 
     $file = 'data.txt';   
     
     file_put_contents($file, $marque . PHP_EOL , FILE_APPEND);
