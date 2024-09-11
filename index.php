a860d098d426c3f42d3102ddc9e2a57eda83f731
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-09-11 11:54:32 +0300

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $text = readline("entrer une texte : "); //readline() recupère le text et met dans le variable text 
+    $personne = readline("entrer un nom : "); //readline() recupère le text et met dans le variable text 
     $file = 'test.txt';   
     
-    file_put_contents($file, $text, FILE_APPEND);
+    file_put_contents($file, $personne, FILE_APPEND);
